b10b488336fb020984dec68f6baad88470a57d6e
joy <joyagbo49@gmail.com>
2023-09-01 02:06:08 +0100

    made changes to route

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
+const routes = require('./routes');
+
+const FeedbackService = require('./services/FeedbackService');
+const SpeakerService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakerService = new SpeakerService('./data/speakers.json');
 
 const app = express();
-
-
 const port = 3000;
 
-app.use(express.static(path.join(__dirname,'./static')));
+app.set('trust proxy', 1);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/index.html'));
-});
-app.get('/speakers', (req, res) => {
-    res.sendFile(path.join(__dirname, './static/speakers.html'));
-  });
+app.use(cookieSession({
+  name: 'session',
+  keys: ['Ghjkl12345','rgdfggdffssffggfffssss']
+}));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+app.use(express.static(path.join(__dirname,'./static')));
+app.use('/',routes({
+  feedbackService,
+  speakerService,
+})
+);
+
+
 
 app.listen(port, () => {   
   console.log(`Server running on port${port}`);
