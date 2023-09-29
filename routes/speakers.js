f3addb21328eb7c5a8dd1410e9099564fb840d33
joy <joyagbo49@gmail.com>
2023-09-29 05:04:39 +0100

    updated files

--- routes/speakers.js
+++ routes/speakers.js
@@ -4,16 +4,12 @@ const router = express.Router();
 module.exports = params => {
 
  const{speakerService} = params;
-  router.get('/', async(req, res) => {
-    const speakers = await speakerService.getList();
-    return res.json(speakers);
+  router.get('/', async (req, res) => {
+    const speakers = await speakerService.getList()
+    res.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers });
   });
-
   
-  router.get('/:shortname', (req, res) => {
-return res.send(`Detail page of ${req.params.shortname}`);
-
-  });
+  router.get('/:shortname', (req, res) => res.send(`Detail page of ${req.params.shortname}`));
 
   return router;
 };
